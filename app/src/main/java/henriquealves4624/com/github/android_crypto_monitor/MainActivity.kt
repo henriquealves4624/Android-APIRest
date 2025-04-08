7cac6aeb791821dfcf57fd9dd918a324d0dd6f03
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-07 22:32:10 -0300

    Preparando o layout para a comunicação entre as APIs

--- app/src/main/java/henriquealves4624/com/github/android_crypto_monitor/MainActivity.kt
+++ app/src/main/java/henriquealves4624/com/github/android_crypto_monitor/MainActivity.kt
@@ -1,20 +1,22 @@
 package henriquealves4624.com.github.android_crypto_monitor
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import henriquealves4624.com.github.android_crypto_monitor.ui.theme.Android_crypto_monitorTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
     }
 }
