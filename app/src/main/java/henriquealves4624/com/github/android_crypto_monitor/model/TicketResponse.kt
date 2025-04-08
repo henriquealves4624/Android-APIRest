26e69872849da850472b73ee70afe627480b205f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-07 22:37:16 -0300

    Adicionando a TicketResponse

---
+++ app/src/main/java/henriquealves4624/com/github/android_crypto_monitor/model/TicketResponse.kt
@@ -0,0 +1,17 @@
+package henriquealves4624.com.github.android_crypto_monitor.model
+
+
+class TickerResponse(
+    val ticker: Ticker
+)
+
+class Ticker(
+    val high: String,
+    val low: String,
+    val vol: String,
+    val last: String,
+    val buy: String,
+    val sell: String,
+    val date: Long
+)
+
